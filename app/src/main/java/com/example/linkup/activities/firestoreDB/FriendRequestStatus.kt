a1363b634108b6ca06fd6a7720454d584d4b967d
antomich04 <antomich04@gmail.com>
2025-04-18 01:30:49 +0300

    make signup layout responsive

--- app/src/main/java/com/example/linkup/activities/firestoreDB/FriendRequestStatus.kt
+++ app/src/main/java/com/example/linkup/activities/firestoreDB/FriendRequestStatus.kt
@@ -4,12 +4,4 @@ enum class FriendRequestStatus(val value: String) {
     PENDING("pending"),
     ACCEPTED("accepted"),
     REJECTED("rejected");
-
-    //Used to convert status given from Firestore to the corresponding enum
-    companion object {
-        fun fromValue(value: String): FriendRequestStatus {
-            return FriendRequestStatus.entries.find { it.value == value.lowercase() }
-                ?: throw IllegalArgumentException("Unknown FriendRequestStatus: $value")
-        }
-    }
 }
